7b0d591eb72cf3ddae72d4d0fbc25f47f37e732a
Nayeemmnt999 <nayeem.mnt999@gmail.com>
2023-07-17 09:23:17 +0600

    style update

--- src/Pages/Home/HomeNews.js
+++ src/Pages/Home/HomeNews.js
@@ -1,25 +1,43 @@
 import React from 'react';
 import { Image } from 'react-bootstrap';
 import Card from 'react-bootstrap/Card';
+import { FaRegBookmark, FaShareAlt } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
+
 
 const HomeNews = ({ news }) => {
 
-    const { _id, rating, total_view, title, author, img, details } = news;
+    const { _id, rating, total_view, title, author, img, details, image_url } = news;
 
     return (
         <Card className="text-center mb-3">
-            <Card.Header>
-                <Image
-                    className=' me-2'
-                    src={author.img}
-                    roundedCircle
-                    style={{ height: "60px "}}
-                />
-                {author.name}
+            {/* section first  */}
+            <Card.Header className='p-3 d-flex justify-content-between align-items-center'>
+                <div className='d-flex align-items-center'>
+                    <div>
+                        <Image
+                            className=' me-2'
+                            src={author.img}
+                            roundedCircle
+                            style={{ height: "50px " }}
+                        />
+                    </div>
+                    <div className=' text-start'>
+                        <p className='fw-semibold mb-0'>{author.name}</p>
+                        <p className='text-body-tertiary mb-0 '>{ author.published_date} </p>
+                    </div>
+                </div>
+
+                {/* second secion  */}
+                <div>
+                    <FaRegBookmark className=' me-2'></FaRegBookmark>
+                    <FaShareAlt></FaShareAlt>
+                </div>
+
             </Card.Header>
             <Card.Body>
                 <Card.Title>{title}</Card.Title>
+                <Card.Img variant="top" src={image_url} />
                 <Card.Text>
                     {
                         details.length > 200 ?
